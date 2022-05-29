f3e38255bde4c9e051091fcef92c4835204855c7
Fathurrahman509 <rahman12ahm4n@gmail.com>
2022-05-29 22:28:24 +0700

    all contacts setup

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,5 +6,6 @@ const router = require('express').Router();
 router.post('/register',register);
 router.post('/login',login);
 router.post('/setAvatar/:id',setAvatar);
+router.get('/allusers/:id', getAllUsers);
 
 module.exports = router;
