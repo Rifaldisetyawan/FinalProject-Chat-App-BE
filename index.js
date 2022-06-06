7f9ec59aa5182dde3b556cc35a7edfc83e3c2ae7
unknown <rifaldisetyawan21@gmail.com>
2022-06-07 01:23:00 +0700

    aaa

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,14 +25,13 @@ mongoose
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('Final project/chat-app/build'));
+if(process.env.NODE.ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'chat-app', 'build')));
 
-  // Express serve up index.html file if it doesn't recognize route
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'chat-app', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'chat-app', 'build', 'index.html'))
   });
+  
 }
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
